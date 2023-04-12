60bfc90d24b7f20c3d5ce0deb1c0d248e5960c72
mihajlovskakate <mihajlovskakate1@gmail.com>
2023-04-12 14:10:01 +0200

    Updates

---
+++ precache-manifest.0382dc9734f96999c4d6237905a7c917.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "15ffaa80582307b3e350",
+    "url": "/book-keeper-app/static/js/main.15ffaa80.chunk.js"
+  },
+  {
+    "revision": "4b138049f41fdca44c70",
+    "url": "/book-keeper-app/static/js/1.4b138049.chunk.js"
+  },
+  {
+    "revision": "f26fcec9aa7f811fa503",
+    "url": "/book-keeper-app/static/js/runtime~main.f26fcec9.js"
+  },
+  {
+    "revision": "12a02c0f7ceae357045a1c5bf7761b6d",
+    "url": "/book-keeper-app/index.html"
+  }
+];
